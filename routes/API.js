ff200a9c68ddc86f48906d7d8b593def5bb36960
Gerry Paul <gpaul12@nc.rr.com>
2023-04-10 10:29:55 -0400

    post notes

--- routes/API.js
+++ routes/API.js
@@ -7,3 +7,16 @@ route.get("/notes", (req, res) => {
   const savedNotes = db;
   res.json(savedNotes);
 });
+route.post("/notes", (req, res) => {
+  const savedNotes = db;
+  const newNote = req.body;
+
+  savedNotes.push(newNote);
+  console.log(savedNotes);
+
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(savedNotes)
+  );
+  res.status(200).json(savedNotes);
+});
