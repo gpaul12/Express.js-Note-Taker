57c2a214471935f5d29e055dbb9f88a9eccaed47
Gerry Paul <gpaul12@nc.rr.com>
2023-04-10 10:29:27 -0400

    get notes

--- routes/API.js
+++ routes/API.js
@@ -2,3 +2,8 @@ const route = require("express").Router();
 const path = require("path");
 const db = require("../db/db.json");
 const fs = require("fs");
+
+route.get("/notes", (req, res) => {
+  const savedNotes = db;
+  res.json(savedNotes);
+});
