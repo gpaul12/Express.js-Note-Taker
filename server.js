b5e277d9c783d79e98fbd5cc347545547ef07642
Gerry Paul <gpaul12@nc.rr.com>
2023-04-10 10:23:10 -0400

    PORT and app

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require("express");
 
 const api = require("./routes/API");
 const htmlRoutes = require("./routes/htmlRoutes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
