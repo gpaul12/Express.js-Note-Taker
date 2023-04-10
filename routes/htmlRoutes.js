44c2afd9a886a1f64f0024178d627d31f03ff9c3
Gerry Paul <gpaul12@nc.rr.com>
2023-04-10 10:32:02 -0400

    get all to index.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,3 +4,7 @@ const route = require("express").Router();
 route.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+
+route.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
