2ec26463361178cc9c64ce71762eddca872c5c76
Gerry Paul <gpaul12@nc.rr.com>
2023-04-10 10:25:51 -0400

    use api & htmlRoutes

--- server.js
+++ server.js
@@ -9,3 +9,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/api", api);
+app.use("/", htmlRoutes);
