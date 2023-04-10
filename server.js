876f3b8bdb36898d0afb171ca94a22b6bdc06ef9
Gerry Paul <gpaul12@nc.rr.com>
2023-04-10 10:24:53 -0400

    middleware

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
